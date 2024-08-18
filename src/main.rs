10dfdc833a0414522c29d958663a4e515ffb8b70
aribambang <aribk210@gmail.com>
2024-08-18 07:37:07 +0700

    add function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::{string, usize};
+
+use rand::{thread_rng, seq::SliceRandom};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card)
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng)
+    }
+
+    fn deal(&mut self, num: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+    // probably need to add error handling!!
+    let cards = deck.deal(3);
+    
+    println!("Here your hand: {:#?}", cards);
+    println!("Here your deck: {:#?}", deck);
 }
